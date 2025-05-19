716533e30f555c2aeaf1917f32995629a4e03100
KatherinaNasraniBarus <katherinanasrani@gmail.com>
2025-05-19 23:45:58 +0700

    lop u

--- template/navbar.php
+++ template/navbar.php
@@ -13,7 +13,7 @@
     <ul class="navbar-nav ml-auto">
       <li class="nav-item dropdown">
         <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
-            <?= 'Admin' ?><i class="fas fa-user-cog ml-2"></i>
+            <?= userLogin()['username'] ?><i class="fas fa-user-cog ml-2"></i>
         </a>
         <div class="dropdown-menu dropdown-menu dropdown-menu-right"> 
             <a href="#" class="dropdown-item text-right">
@@ -24,6 +24,7 @@
                 Log Out <i class="fas fa-sign-out-alt"></i>
             </a>
         </div>
+
       </li>
       
     </ul>
