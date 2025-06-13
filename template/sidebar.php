ccc459dcc9930d8f28cdded12ddd61b5ebfc88d1
DamaiSilalahi <damailalahi0406@gmail.com>
2025-06-13 11:37:08 +0700

    y

--- template/sidebar.php
+++ template/sidebar.php
@@ -2,7 +2,7 @@
  <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="<?= $main_url ?>dashboard.php" class="brand-link">
-      <img src="<?= $main_url ?>asset/image/logo.jpg" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="<?= $main_url ?>asset/image/logo.png" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Stok Barang</span>
     </a>
 
@@ -14,7 +14,7 @@
           <img src="<?= $main_url?>asset/image/<?= userLogin()['foto'] ?>" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block"><?= 'aby sanara'?></a>
+          <a href="#" class="d-block"><?= 'Katrin'?></a>
         </div>
       </div>
 
